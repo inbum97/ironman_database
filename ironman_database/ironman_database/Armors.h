db552fb1a77d8306648ede5fcaac4a431224a143
Benny Kusuma <bennytandio@hotmail.com>
2019-06-11 15:04:03 -0700

    updated (1.1) : 1. Armors.h 2. main.cpp

--- ironman_database/ironman_database/Armors.h
+++ ironman_database/ironman_database/Armors.h
@@ -9,30 +9,54 @@
 
 using namespace std;
 
-template <typename T>
-class Armor
+class Armors
 {
 private:
 	string codename;	// Codename (name of armor) (unique key)
 	string armorType;	// Type (amor type) (secondary key)
 	string creator;	// Creator 
 	int yearMade;	// Year Made - int (year of creation)
-	vector<T> users;	// Users - vector of string (multiple users)
+	string users;	// Users - vector of string (multiple users)
 	string movieAppeared;	// movieAppeared - string (Name of the movies appeared)
 	string currStats;	// Current Status (active, damaged, destroyed)
-	vector<T> capabilities;	// Capabilities - vector of string (multiple capabilities)
-	vector<T> weapons;	// Weapons - vector of string (multiple weapons)
+	string capabilities;	// Capabilities - vector of string (multiple capabilities)
+	string weapons;	// Weapons - vector of string (multiple weapons)
 	string precede;	// Precede - string (precede model)
 	string succeed;	// Succeed - string (succeeding model)
 
 public:
 	// Constructors
-	Armor();
-	Armor(string coden, string type, string inventor, int yearmade, vector<T> user, string movieShown,
-		string currStat, vector<T> capbl, vector<T> weap, string precedeModel, string succeedModel);
+	Armors()
+	{
+		codename = "";
+		armorType = "";
+		creator = "";
+		yearMade = 0;
+		users = "";
+		movieAppeared = "";
+		currStats = "";
+		capabilities = "";
+		weapons = "";
+		precede = "";
+		succeed = "";
+	};
+
+	Armors(string coden, string type, string inventor, int yearmade, 
+		string user, string movieShown, string currStat, string capbl, 
+		string weap, string precedeModel, string succeedModel) : codename(coden), 
+																	armorType(type), 
+																	creator(inventor),
+																	yearMade(yearmade),
+																	users(user),
+																	movieAppeared(movieShown),
+																	currStats(currStat),
+																	capabilities(capbl),
+																	weapons(weap),
+																	precede(precedeModel),
+																	succeed(succeedModel) {};
 
 	// Destructor
-	~Armor();
+	//~Armor();
 
 	// Setters
 
@@ -45,39 +69,5 @@ public:
 
 };
 
-template <typename T>
-Armor<T>::Armor()
-{
-	seriesNum = 0;
-	codename = "";
-	creator = "";
-	yearMade = 0;
-	users.clear();
-	movieAppeared = "";
-	currStat = "";
-	capabilities.clear();
-	weapons.clear();
-	precede = "";
-	succeed = "";
-}
-
-template <typename T>
-Armor<T>::Armor(string coden, string type, string inventor, int yearmade, vector<T> user, string movieShown,
-	string currStat, vector<T> capbl, vector<T> weap, string precedeModel, string succeedModel)
-{
-	codename = coden;
-	armorType = type;
-	creator = inventor;
-	yearMade = yearmade;
-	users = user;
-	movieAppeared = movieShown;
-	currStats = currStat;
-	capabilities = capbl;
-	weapons = weap;
-	precede = precedeModel;
-	succeed = succeedModel;
-}
-
 #endif
-#pragma once
 #pragma once
--- ironman_database/ironman_database/main.cpp
+++ ironman_database/ironman_database/main.cpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+
+#include "Armors.h"
 
 using namespace std;
 
@@ -14,21 +18,106 @@ void fileInput(string filename);
 int main()
 {
 	const char inputFileName[] = "armors.txt";
+	fileInput(inputFileName);
 
-	cout << "Hello World!\n";
 }
 
 void fileInput(string filename)
 {
 	ifstream infile(filename);
 
-	string codename, type, creator, movie, curstat, precede, succeed;
-	int year;
+	string codename, type, creator, movie, curstat, precede, succeed, users, capbl, weap;
+	string item="", item2="";
+	int year = 0;
 
 	if (!infile) {
 		cout << "Error happened to open the input file!" << endl;
 		exit(EXIT_FAILURE);
 	}
+
+	/*
+	int i = 0;
+	while (getline(infile, codename, ';'))
+	{
+		cout << codename << endl;
+	}
+	*/
+
+	/*
+	// read the amor user into armor vector
+	getline(infile, item, ',');
+	users.push_back(item);
+	getline(infile, item, ';');
+	users.push_back(item);
+
+	// CAPBILITIES
+	// (1) read from the file until the ';' as one whole string,
+	// (2) and the split them into pieces of string and then push them into its vector
+	getline(infile, item, ';'); // (1)
+	size_t comma = item.find_first_of(','); // first comma
+	size_t nextComma = item.find_first_of(',', comma + 1); // next comma
+	item2 = item.substr(0, comma); // this line onwards is (2)
+	capbl.push_back(item2);
+	while (comma != string::npos)
+	{
+		item2 = item.substr(comma + 1, nextComma - comma - 1);
+		capbl.push_back(item2);
+		comma = nextComma;
+		nextComma = item.find_first_of(',', nextComma + 1);
+	}
+
+	// WEAPONS
+	// (1) read from the file until the ';' as one whole string,
+	// (2) and the split them into pieces of string and then push them into its vector
+	getline(infile, item, ';'); // (1)
+	comma = item.find_first_of(','); // first comma
+	nextComma = item.find_first_of(',', comma + 1); // next comma
+	item2 = item.substr(0, comma); // this line onwards is (2)
+	weap.push_back(item2);
+	while (comma != string::npos)
+	{
+		item2 = item.substr(comma + 1, nextComma - comma - 1);
+		weap.push_back(item2);
+		comma = nextComma;
+		nextComma = item.find_first_of(',', nextComma + 1);
+	}
+	*/
+
+	
+	// READING THE FILE AND DECLARE TO RESPECTICE VARIABLE 
+	//======================================================
+	while (getline(infile, codename, ';'))
+	{
+		getline(infile, type, ';');
+		getline(infile, creator, ';');
+		infile >> year;
+		infile.ignore();
+		getline(infile, users, ';');
+		getline(infile, movie, ';');
+		getline(infile, curstat, ';');
+		getline(infile, capbl, ';');
+		getline(infile, weap, ';');
+		getline(infile, precede, ';');
+		getline(infile, succeed, ';');
+
+
+		// TEST IF FILE IS READ PROPERLY
+		cout << "======================================================" << endl;
+		cout << "codename: " << codename << endl;
+		cout << "type: " << type << endl;
+		cout << "creator: " << creator << endl;
+		cout << "year: " << year << endl;
+		cout << "users: " << users << endl;
+		cout << "movie: " << movie << endl;
+		cout << "status: " << curstat << endl;
+		cout << "capabilities: " << capbl << endl;
+		cout << "weapons: " << weap << endl;
+		cout << "precede: " << precede << endl;
+		cout << "succeed: " << succeed << endl;
+		cout << "======================================================" << endl << endl;
+	}
+	
+	
 
 	/*
 
